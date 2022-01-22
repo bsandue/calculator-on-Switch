dc3c4d52999b618b69962c22e128b6abb33fadc3
bsandue <zh.s.beisbaevy@mail.ru>
2022-01-22 14:53:37 +0600

    feat: added output to the console

--- main.js
+++ main.js
@@ -20,3 +20,12 @@ function calc(c, a, b){
 										break;
 	}
 }
+
+console.log(calc('+', 2, 2));
+console.log(calc('-', 2, 2));
+console.log(calc('/', 2, 2));
+console.log(calc('*', 2, 2));
+console.log(calc('%', 2, 2));
+console.log(calc('', '', ''));
+console.log(calc('5', '2', '2'));
+console.log(calc('+', 's', 'd'));
