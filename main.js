45e38bdcb30cd07125b85724807ce745bdc4ecdf
bsandue <zh.s.beisbaevy@mail.ru>
2022-01-22 14:50:15 +0600

    feat: added mathematical operators

--- main.js
+++ main.js
@@ -3,5 +3,20 @@ function calc(c, a, b){
 	switch (c){
 		case '':
 			return 'unknown operation';
+			case '+':
+				return a + b;
+				case '-':
+					return a - b;
+					case '/':
+						return a / b;
+						case '*':
+							return a * b;
+							case '%':
+								return a % b;
+								case '**':
+									return a ** b;
+									default:
+										return 'Error';
+										break;
 	}
 }
